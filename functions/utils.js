9b8f7c62c44a0c9dff9727db2292e32188f12e1c
PriorityLexusVB <rob.brasco@priorityautomotive.com>
2025-08-12 19:44:46 -0400

    chore: remove unused utils

--- functions/utils.js
+++
@@ -1,7 +0,0 @@
-const getFirst = (prop) => (Array.isArray(prop) ? prop[0] : prop);
-const getText = (prop) => {
-  const val = getFirst(prop);
-  return (val && typeof val === "object") ? (val["#text"] || "") : (val || "");
-};
-
-module.exports = { getFirst, getText };
