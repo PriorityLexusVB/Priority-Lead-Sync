bb4ab9d58ac6869b7b9d41b5828c6f02306d5e3d
PriorityLexusVB <rob.brasco@priorityautomotive.com>
2025-08-15 12:01:21 -0400

    chore: add deploy workflow

--- functions/test/smoke.test.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-
-// Simple smoke test to ensure the test runner is wired up.
-assert.ok(true, 'basic assertion should pass');
-
-console.log('Functions tests passed');
